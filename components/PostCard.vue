38ce5290c6aaa62fe659249403a7b3891a2d76d1
jaisonschmidt <git@stackblitz.com>
2025-04-10 17:02:05 -0300

    Initial commit

---
+++ components/PostCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtLink :to="`/post/${post.slug}`">
+      <h2>{{ post.title }}</h2>
+    </NuxtLink>
+    <p>{{ post.description }}</p>
+    <hr />
+  </div>
+</template>
+
+<script setup>
+defineProps({ post: Object });
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Meu Blog com Nuxt</h1>
+    <PostCard v-for="post in posts" :key="post.id" :post="post" />
+  </div>
+</template>
+
+<script setup>
+import { posts } from '@/data/posts';
+</script>
